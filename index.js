ed6a8c06dda14167fc79101febfb25da607d261c
Mathieu Alain <alain.mathieu1996@gmail.com>
2019-07-02 21:18:30 +0200

    Replaced setInterval by setTimeout

--- index.js
+++ index.js
@@ -22,28 +22,31 @@ let nextRun = config.time_out_negative * seconds;
 discordClient.on('ready', () => {
     //Acknowledge the connection
     Logs.writeLog(`Logged in as ${discordClient.user.tag}!`);
-    //Retrieve the channel where messages will be posted
-    const channel = discordClient.channels.find( chan => chan.name === config.channel);
     //Check the ping every X seconds
-    setInterval(() => {
-        //Check the ping
-        if(discordClient.ping >= config.max_ping){
-            //Post a message in the channel and post a tweet
-            channel.send('Sending a tweet to discord! Ping:' + discordClient.ping);
-            twitterClient.post('statuses/update', {status: new Date().toLocaleString() + ' - ' + config.tweet_msg},  function(error) {
-                if(error){
-                    Logs.writeLog(error.message);
-                    throw error;
-                }
-            });
-            nextRun = config.time_out_positive * seconds;
-            Logs.writeLog(`Ping is to high! Next execution in ` + config.time_out_positive + ' seconds.');
-        }else {
-            nextRun = config.time_out_negative * seconds;
-            Logs.writeLog(`Ping is OK! Next execution in ` + config.time_out_negative + ' seconds.');
-        }
-    }, nextRun);
+    setTimeout(checkPing, nextRun);
 });
 
 //Login the bot
 discordClient.login(config.discord_token);
+
+//Function to check the ping
+let checkPing = function() {
+    //Retrieve the channel where messages will be posted
+    let channel = discordClient.channels.find( chan => chan.name === config.channel);
+    if(discordClient.ping >= config.max_ping){
+        //Post a message in the channel and post a tweet
+        channel.send('Sending a tweet to discord! Ping:' + discordClient.ping);
+        twitterClient.post('statuses/update', {status: new Date().toLocaleString() + ' - ' + config.tweet_msg},  function(error) {
+            if(error){
+                Logs.writeLog(error.message);
+                throw error;
+            }
+        });
+        nextRun = config.time_out_positive * seconds;
+        Logs.writeLog(`Ping is to high! Next execution in ` + config.time_out_positive + ' seconds.');
+    }else {
+        nextRun = config.time_out_negative * seconds;
+        Logs.writeLog(`Ping is OK! Next execution in ` + config.time_out_negative + ' seconds.');
+    }
+    setTimeout(checkPing, nextRun);
+};
